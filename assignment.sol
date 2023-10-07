5549dceb0f674f0eb19efdcd1123510e1dd8cd73
4Noyis <berenalpb@outlook.com>
2023-10-07 23:39:17 +0300

    commit

---
+++ assignment.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract mainContract{
+
+    struct Account{
+        address addr;
+        string name;
+    }
+    
+
+    Account[] public admins;
+
+    function getAllAdmins() public view returns(Account[] memory){
+        Account[] memory _admins = new Account[](admins.length);
+        // mevcut admins dizisinin uzunluğunda bir _admins dizisi oluşturulur.
+
+        for (uint256 i = 0; i < admins.length; i++) {
+            _admins[i]= admins[i];
+        }
+        return _admins;
+    }
+
+}
